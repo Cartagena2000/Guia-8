218edcbfddc60c693b4f184197a8fbfcbeccbc5e
Cartagena2000 <144555706+Cartagena2000@users.noreply.github.com>
2024-09-08 21:55:33 -0600

    guia

---
+++ ProductoAPI/Migrations/20240827000933_Migracion inicial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProductoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracioninicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Categoria = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Categoria", "Descripcion", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, "Electrónica", "Una laptop de lato rendimiento.", "Laptop" },
+                    { 2, "Electrónica", "Un smartphone de nueva generacion.", "Smartphone" },
+                    { 3, "Muebles", "Una silla de escritorio cómoda.", "Silla de escritorio" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ ProductoAPI/Models/ProductoContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProductoAPI.Models
+{
+    public class ProductoContext : DbContext
+    {
+        public ProductoContext(DbContextOptions<ProductoContext> options) 
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Producto> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Siembra de datos de productos
+            modelBuilder.Entity<Producto>().HasData(
+                new Producto
+                {
+                    Id = 1,
+                    Nombre = "Laptop",
+                    Categoria = "Electrónica",
+                    Descripcion = "Una laptop de lato rendimiento."
+                },
+                new Producto
+                {
+                    Id = 2,
+                    Nombre = "Smartphone",
+                    Categoria = "Electrónica",
+                    Descripcion = "Un smartphone de nueva generacion."
+                },
+                new Producto
+                {
+                    Id = 3,
+                    Nombre = "Silla de escritorio",
+                    Categoria = "Muebles",
+                    Descripcion = "Una silla de escritorio cómoda."
+                }
+             );
+        }
+    }
+}
